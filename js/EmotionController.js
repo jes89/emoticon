572e9f5633bc35ecc21e3acbd580ac6dc53a9b58
jes89 <hiter00@naver.com>
2019-01-17 16:26:13 +0900

    first commit

---
+++ js/EmotionController.js
@@ -0,0 +1,165 @@
+﻿;
+
+var EmotionController = (function () {
+
+    var EmotionController = function (iframeSrc, targetEl, toggleBtn, contentsArea) {
+
+        if (iframeSrc == null || iframeSrc.length == 0) {
+            alert("아이프레임 주소가 입력되지 않았습니다.\n 모바일 주소: /emoticon/m/index.html \n PC 주소 : /emoticon/index.html");
+            return;
+        }
+
+        if (targetEl == null || targetEl.nodeType != 1) {
+            alert("iframe을 붙일 엘리먼트가 지정되지 않았습니다. \n document.getElementById를 이용해 지정해주세요.");
+            return;
+        }
+        
+        if (toggleBtn == null || toggleBtn.nodeType != 1) {
+            alert("이모티콘 보기/접기 버튼 엘리먼트가 지정되지 않았습니다. \n document.getElementById를 이용해 지정해주세요.");
+            return;
+        }
+
+        if (contentsArea == null) {
+            alert("이모티콘을 붙일 영역이 지정되지 않았습니다. document.getElementById를 이용해 지정해주세요.");
+            return;
+        }
+
+        this.iframeSrc = iframeSrc;
+
+        EmotionController.prototype.toggleBtn = toggleBtn;
+        EmotionController.prototype.targetEl = targetEl;
+        EmotionController.prototype.contentsArea = contentsArea;
+        EmotionController.prototype.targetRange = null;
+    }
+
+    EmotionController.prototype.createFrame = function () {
+       
+        var emoticonObj = null;
+        var targetEl = this.targetEl;
+        var iframeSrc = this.iframeSrc;
+        var emotionIframe = document.createElement('iframe');
+
+        emotionIframe.src = iframeSrc;
+
+        emotionIframe.style.cssText = "width:100%; height:100%;";
+        
+        targetEl.style.display = "none";
+
+        targetEl.appendChild(emotionIframe);
+
+        toggleEmotion(this);
+
+        setContentsDiv(EmotionController.prototype.contentsArea);
+
+    }
+
+    EmotionController.prototype.placeCaretAtEnd = function (el) {
+        el.focus();
+
+        if (typeof window.getSelection != "undefined" && typeof document.createRange != "undefined") {
+            var range = document.createRange();
+            range.selectNodeContents(el);
+            range.collapse(false);
+            var sel = window.getSelection();
+            sel.removeAllRanges();
+            sel.addRange(range);
+        } else if (typeof document.body.createTextRange != "undefined") {
+            var textRange = document.body.createTextRange();
+            textRange.moveToElementText(el);
+            textRange.collapse(false);
+            textRange.select();
+        }
+
+        saveRange(EmotionController.prototype.contentsArea);
+    }
+
+
+    var setContentsDiv = function (contentsDiv) {
+
+        contentsDiv.addEventListener("focus", function(){saveRange(event.target)});
+        contentsDiv.addEventListener("click", function(){saveRange(event.target)});
+        contentsDiv.addEventListener("keyup", function(){saveRange(event.target)});
+        contentsDiv.setAttribute("contenteditable", true);
+    }
+
+    function saveRange(that) {
+
+        if (that.nodeName === "DIV" && that.id === "contentsArea") {
+            var sel = window.getSelection();
+            if (sel.rangeCount > 0) {
+                EmotionController.prototype.targetRange = sel.getRangeAt(0);
+            } else {
+                setTimeout(function () {
+                    //saveRange(this);
+                    saveRange(that);
+                }, 100)
+            }
+
+        }
+
+    }
+
+    var toggleEmotion = function (that) {
+
+        var toggleBtn = that.toggleBtn;
+        var targetEl = that.targetEl;
+        
+        toggleBtn.addEventListener("click", function () {
+
+            var displayState = targetEl.style.display;
+            var parentEl = this.parentElement;
+            if (displayState === "none") {
+                targetEl.style.display = "block";
+                parentEl.classList.add("active");
+            } else {
+                targetEl.style.display = "none";
+                parentEl.classList.remove("active");
+            }
+        })
+    }
+
+    EmotionController.prototype.attachEmoticon = function (iframeSrc, width, height) {
+
+        var cnt = 0;
+        var maxWaitCnt = 100;
+        var insertIframe = document.createElement('iframe');
+        var contentsArea = EmotionController.prototype.contentsArea;
+        var targetRange = null;
+
+        setTimeout(function timmer() {
+
+            targetRange = EmotionController.prototype.targetRange;
+
+            if (targetRange == null) {
+
+                EmotionController.prototype.placeCaretAtEnd(contentsArea);
+
+                if (cnt >= maxWaitCnt) {
+                    alert("이모티콘을 첨부하는 동안 오류가 발생했습니다. 관리자에게 문의하세요.");
+                    return;
+                }
+
+                setTimeout(function () {
+                    timmer();
+                    cnt++;
+                }, 100)
+
+            } else {
+                insertIframe.src = iframeSrc;
+                insertIframe.scrolling = "no";
+                insertIframe.frameborder = 0;
+                insertIframe.style.width = width;
+                insertIframe.style.height = height;
+
+                targetRange.insertNode(insertIframe);
+                targetRange.setStartAfter(insertIframe);
+
+            }
+        }, 100);
+    }
+
+
+    return EmotionController;
+
+})();
+
---
+++ js/Mobile.js
@@ -0,0 +1,243 @@
+﻿;
+
+var MobileEmoticon = (function () {
+    
+    var that = this;
+
+    var MobileEmoticon = function (bodyEl) {
+        MobileEmoticon.prototype.bodyEl = bodyEl;
+        getEmotionData();
+    }
+
+    MobileEmoticon.prototype.emotionStore = null;
+    MobileEmoticon.prototype.emotionUl = null;
+    MobileEmoticon.prototype.emocticonInsertDiv = null;
+
+    MobileEmoticon.prototype.init = function () {
+        
+        var maxCnt = 1000;
+        var cnt = 0;
+        var that = this;
+  
+        setTimeout(function timmer() {
+
+            if (MobileEmoticon.prototype.emotionStore) {
+                that.initLayout();
+            } else {
+                if (maxCnt < cnt) {
+                    alert("이모티콘을 다운로드하는 중 오류가 발생했습니다.");
+                } else {
+                    setTimeout(function () {
+                        cnt++;
+                        timmer();
+                    }, 100);
+                }
+            }
+        },100);
+    }
+
+    MobileEmoticon.prototype.initLayout = function (emotionArr) {
+
+        var bodyEl = MobileEmoticon.prototype.bodyEl;
+        var emotiWrap = document.createElement("div");
+        var imotiThumb = document.createElement("div");
+        var emoticonMobile = document.createElement("div");
+        var emocticonInsertDiv = document.createElement("div");
+        var emotionUl = document.createElement("ul");
+
+        emotiWrap.className = "emotiWrap";
+        imotiThumb.className = "imotiThumb";
+        emocticonInsertDiv.className = "emocticon-insert";
+        emoticonMobile.className = "emoticon-mobile";
+
+        emocticonInsertDiv.style.display = "none";
+
+        emotiWrap.appendChild(imotiThumb);
+        emotiWrap.appendChild(emoticonMobile);
+        emotiWrap.appendChild(emocticonInsertDiv);
+        emoticonMobile.appendChild(emotionUl);
+
+        bodyEl.appendChild(emotiWrap);
+
+        MobileEmoticon.prototype.emotionUl = emotionUl;
+        MobileEmoticon.prototype.emocticonInsertDiv = emocticonInsertDiv;
+
+        this.appendEmotionGroupIcons(emotiWrap, imotiThumb);
+        this.appendEmotions();
+    }
+
+    MobileEmoticon.prototype.appendEmotionGroupIcons = function (emotiWrap, imotiThumb) {
+
+        var emotionStore = MobileEmoticon.prototype.emotionStore;
+        var imotiThumbUl = document.createElement("ul");
+        var tempData = null;
+        var tempEmotionli = null;
+        var tempImg = null;
+
+        for (var ix = 0, ixLen = emotionStore.length; ix < ixLen; ix++) {
+
+            tempData = emotionStore[ix];
+
+            tempEmotionli = document.createElement("li");
+            tempImg = document.createElement("img");
+
+            tempImg.src = tempData["emoThumb"];
+
+            if (ix === 0) {
+                tempEmotionli.classList.add("on")
+            }
+
+            tempEmotionli.setAttribute("data-groupIdx", tempData.emoIdx);
+            tempEmotionli.setAttribute("data-idx", ix);
+            
+            this.addEmotionsGroupsClickEvent(imotiThumbUl, tempEmotionli);
+            
+            tempEmotionli.appendChild(tempImg);
+            imotiThumbUl.appendChild(tempEmotionli);
+        }
+
+        imotiThumb.appendChild(imotiThumbUl);  
+    }
+
+    MobileEmoticon.prototype.appendEmotions = function (selectedEl) {
+
+        var groupIdx = selectedEl == null ? 0 : selectedEl.getAttribute("data-groupIdx");
+        var idx = selectedEl == null ? 0 : selectedEl.getAttribute("data-idx");
+        var emotionObj = MobileEmoticon.prototype.emotionStore[idx];
+        var emotionPositions = emotionObj["itemsMobileThumbPos"];
+        var positionsArr = emotionPositions["positions"];
+        var POSITIONX = 0;
+        var POSITIONY = 1;
+        var tempData = null;
+        var tempEmotionli = null;
+        var tempButton = null;
+        var emotionUl = MobileEmoticon.prototype.emotionUl;
+        var width = null;
+        var height = null;
+
+        if (selectedEl) {
+            emotionUl.innerHTML = "";
+        }
+
+        for (var ix = 0, ixLen = positionsArr.length; ix < ixLen; ix++) {
+
+            tempEmotionli = document.createElement("li");
+            tempButton = document.createElement("span");
+
+            width = emotionPositions["width"];
+            height = emotionPositions["height"];
+
+            tempButton.style.cssText = "background: url('" + emotionObj["itemsMobileThumb"] + "') " + positionsArr[ix][POSITIONX] + "% " + positionsArr[ix][POSITIONY] + "% no-repeat; background-size: 100vw;"
+    
+
+            tempEmotionli.setAttribute("data-emoIdx", emotionObj.emoIdx);
+            tempEmotionli.setAttribute("data-itemIdx", ix);
+            tempEmotionli.setAttribute("data-width", width);
+            tempEmotionli.setAttribute("data-height", height);
+
+            tempEmotionli.appendChild(tempButton);
+            emotionUl.appendChild(tempEmotionli);
+
+            tempEmotionli.addEventListener("click", showPreviewEmoticon);
+        }
+
+    }
+
+
+    MobileEmoticon.prototype.addEmotionsGroupsClickEvent = function (imotiThumbUl, emotionli) {
+        emotionli.addEventListener("click", function () {
+            removeOnClass(imotiThumbUl);
+            addOnClass(this);
+            MobileEmoticon.prototype.appendEmotions(this);
+        })
+    }
+
+    var removeOnClass = function (imotiThumbUl) {
+
+        var emotionGroupsArr = imotiThumbUl.children;
+        var tempLi = null;
+
+        for (var ix = 0, ixLen = emotionGroupsArr.length; ix < ixLen; ix++) {
+            tempLi = emotionGroupsArr[ix];
+            if(tempLi.classList.contains("on")){
+                tempLi.classList.remove("on");
+                break;
+            }
+        }
+    }
+
+    var addOnClass = function (that) {
+        that.classList.add("on");
+    }
+
+   
+    var showPreviewEmoticon = function () {
+
+        var emocticonInsertDiv = MobileEmoticon.prototype.emocticonInsertDiv;
+        var selectedEmoticonImg = document.createElement("div");
+        var closeBtn = document.createElement("button");
+        var emoIdx = this.getAttribute("data-emoIdx");
+        var itemIdx = this.getAttribute("data-itemIdx");
+        var width = this.getAttribute("data-width") + "px";
+        var height = this.getAttribute("data-height") + "px";
+        var iframeSrc = "http://app.hsp.net/M/Emoticon/EmoticonView.aspx?emoIdx=" + emoIdx + "&itemIdx=" + itemIdx;
+
+        emocticonInsertDiv.innerHTML = "";
+
+        emocticonInsertDiv.style.display = "block";
+        selectedEmoticonImg.style.display = "inline-block";
+
+        closeBtn.type = "button"
+        closeBtn.className = "close";
+        closeBtn.textContent = "닫기";
+  
+        selectedEmoticonImg.style.background = this.children[0].style.background;
+        
+        selectedEmoticonImg.style.width = width;
+        selectedEmoticonImg.style.height = height;
+
+        emocticonInsertDiv.appendChild(selectedEmoticonImg);
+        emocticonInsertDiv.appendChild(closeBtn);
+
+
+        selectedEmoticonImg.addEventListener("click", function () {
+            exportEmotion(iframeSrc, width, height);
+            hidePreview(emocticonInsertDiv);
+            parent.EmotionController.prototype.toggleBtn.click();
+        });
+
+        closeBtn.addEventListener("click", function () {
+            hidePreview(emocticonInsertDiv);
+        });
+
+        var hidePreview = function (emocticonInsertDiv) {
+            emocticonInsertDiv.innerHTML = "";
+            emocticonInsertDiv.style.display = "none";
+        }
+    }
+
+    var exportEmotion = function (iframeSrc, width, height) {
+        parent.EmotionController.prototype.attachEmoticon(iframeSrc, width, height);
+    }
+    
+    var getEmotionData = function () {
+
+        var xhttp = new XMLHttpRequest();
+        var reqDomain = location.host.indexOf("192.168") > -1 ? "http://192.168.3.26:82" : "";
+        var url = reqDomain + "/m/AjaxForm/EmotionList.aspx";
+
+        xhttp.open("GET", url, true);
+        xhttp.send(null);
+
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4) {
+                if (this.status == 200) {
+                    MobileEmoticon.prototype.emotionStore = JSON.parse(this.responseText);
+                }
+            }
+        }
+    }
+
+    return MobileEmoticon;
+
+})();
